f4a52000b7972842b9344a14cf96ac2557d92bc2
George Sephton <43405566+george-sephton@users.noreply.github.com>
2020-04-17 07:23:45 +0100

    added AC057TC1 for testing (2)

---
+++ eink_asf4_1_5/eink/displays/ac057tc1.h
@@ -0,0 +1,149 @@
+/**
+ * \file
+ * 
+ * \brief AC057TC1 Eink Display Service
+ * 
+ * Eink Software Library
+ * Microchip ASF4 Variant - release 1.5 - January 2020
+ * 
+ * \author George Sephton
+ * 
+ * \copyright Copyright (c) 2020 Ineltek Ltd. All rights reserved.
+ * 
+ * \page eink_xplained_license License
+ *
+ * \brief Software License and Disclaimer
+ *
+ * \section software_license Software License
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * For a copy of the GNU General Public License, see
+ * see <https://www.gnu.org/licenses/>
+ *
+ * \subsection software_license_logo Logo Use
+ *
+ * Use of the Ineltek logo, packaged with this software library, is not subject
+ * to the above GPL software license and is property of Ineltek GmbH and it subsidiaries.
+ * It may not be used for commercial use and may not be used to endorse or promote products
+ * without specific prior written permission.
+ *
+ * \subsection software_license_conditions Conditions
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    software license, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    software license, this list of conditions and the following disclaimer
+ *    in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. The names, trademarks or logos of Eink, Ineltek or Microchip, and any combination
+ *    thereof, may not be used to endorse or promote products derived from this
+ *    software without specific prior written permission. They are also subject to their
+ *    own copyrights and trademarks that may not be expressed here.
+ *
+ * \section disclaimer Disclaimer
+ *
+ * THIS SOFTWARE IS PROVIDED BY INELTEK "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL INELTEK BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+#ifndef AC057TC1_H_
+#define AC057TC1_H_
+
+#include <compiler.h>
+
+#include <eink/drivers/eink_driver.h>
+#include <eink/drivers/uc8159/uc8159.h>
+
+#include <eink/eink_gfx/eink_gfx.h>
+#include <eink/eink_gfx/eink_gfx_graphics.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//! \name Fundamental Display defines
+//@{
+#define GFX_AC057TC1_MAX_WIDTH              600
+#define GFX_AC057TC1_MAX_HEIGHT             448
+#define GFX_AC057TC1_PIXELS_PER_BYTE        2
+#define GFX_AC057TC1_DISPLAY_BUFFER_SIZE    ((GFX_AC057TC1_MAX_WIDTH * GFX_AC057TC1_MAX_HEIGHT) / GFX_AC057TC1_PIXELS_PER_BYTE)
+//@}
+
+//! \name AC057TC1 display buffers 
+//@{
+volatile uint8_t *ac057tc1_dtm1_display_buffer;
+//@}
+
+//! \name AC057TC1 display initialization function
+//@{
+void eink_ac057tc1_init(
+        struct uc8159_config *const config,
+        bool clear_display);
+//@}
+
+//! \name AC057TC1 display functions
+//@{
+    
+/**
+ * \brief Send the refresh command to the display driver.
+ */
+static inline void eink_ac057tc1_refresh_display_buffer(void)
+{
+    /* Enable power to the display */
+    eink_write_data(UC8159_PON, 0, 0);
+    
+    /* Send the Refresh Display command */
+    eink_write_data(UC8159_DRF, 0, 0);
+    uc8159_wait_for_busy();
+    
+    /* Disable power to the display */
+    eink_write_data(UC8159_POF, 0, 0);
+    uc8159_wait_for_busy();
+}
+
+void eink_ac057tc1_put_display_buffer(
+        bool refresh_display);
+
+void eink_ac057tc1_set_pixel(
+        eink_coordinate x_set,
+        eink_coordinate y_set,
+        enum eink_pixel_colour pixel_colour);
+
+void eink_ac057tc1_graphics_load_mono_image(
+        uint8_t *img_array,
+        uint16_t array_size,
+        eink_coordinate image_width_px,
+        eink_coordinate image_height_bytes,
+        eink_coordinate x_place,
+        eink_coordinate y_place,
+        enum eink_pixel_colour foreground_colour,
+        enum eink_pixel_colour background_colour);
+//@}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* AC057TC1_H_ */
---
+++ example_projects/eink_asf4_1_5/AC057TC1/samg55_ac057tc1__4_1_5/samg55_ac057tc1__4_1_5/driver_init.c
@@ -0,0 +1,245 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file
+ * to avoid losing it when reconfiguring.
+ */
+
+#include "driver_init.h"
+#include <hal_init.h>
+#include <hpl_pmc.h>
+#include <peripheral_clk_config.h>
+#include <utils.h>
+
+void delay_driver_init(void)
+{
+	delay_init(SysTick);
+}
+
+void system_init(void)
+{
+	init_mcu();
+
+	_pmc_enable_periph_clock(ID_PIOA);
+
+	_pmc_enable_periph_clock(ID_PIOB);
+
+	/* Disable Watchdog */
+	hri_wdt_set_MR_WDDIS_bit(WDT);
+
+	/* GPIO on PA0 */
+
+	// Set pin direction to input
+	gpio_set_pin_direction(EINK_X_BUTTON_0_PIN, GPIO_DIRECTION_IN);
+
+	gpio_set_pin_pull_mode(EINK_X_BUTTON_0_PIN,
+	                       // <y> Pull configuration
+	                       // <id> pad_pull_config
+	                       // <GPIO_PULL_OFF"> Off
+	                       // <GPIO_PULL_UP"> Pull-up
+	                       // <GPIO_PULL_DOWN"> Pull-down
+	                       GPIO_PULL_UP);
+
+	gpio_set_pin_function(EINK_X_BUTTON_0_PIN, GPIO_PIN_FUNCTION_OFF);
+
+	/* GPIO on PA9 */
+
+	// Set pin direction to input
+	gpio_set_pin_direction(EINK_X_BUTTON_2_PIN, GPIO_DIRECTION_IN);
+
+	gpio_set_pin_pull_mode(EINK_X_BUTTON_2_PIN,
+	                       // <y> Pull configuration
+	                       // <id> pad_pull_config
+	                       // <GPIO_PULL_OFF"> Off
+	                       // <GPIO_PULL_UP"> Pull-up
+	                       // <GPIO_PULL_DOWN"> Pull-down
+	                       GPIO_PULL_UP);
+
+	gpio_set_pin_function(EINK_X_BUTTON_2_PIN, GPIO_PIN_FUNCTION_OFF);
+
+	/* GPIO on PA10 */
+
+	gpio_set_pin_level(EINK_X_LED_2_PIN,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   true);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(EINK_X_LED_2_PIN, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(EINK_X_LED_2_PIN, GPIO_PIN_FUNCTION_OFF);
+
+	/* GPIO on PA11 */
+
+	gpio_set_pin_level(EINK_CSB,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(EINK_CSB, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(EINK_CSB, GPIO_PIN_FUNCTION_OFF);
+
+	/* GPIO on PA12 */
+
+	gpio_set_pin_level(EINK_SPI_SDA,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(EINK_SPI_SDA, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(EINK_SPI_SDA, GPIO_PIN_FUNCTION_OFF);
+
+	/* GPIO on PA13 */
+
+	gpio_set_pin_level(EINK_RST,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(EINK_RST, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(EINK_RST, GPIO_PIN_FUNCTION_OFF);
+
+	/* GPIO on PA14 */
+
+	gpio_set_pin_level(EINK_SPI_SCL,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(EINK_SPI_SCL, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(EINK_SPI_SCL, GPIO_PIN_FUNCTION_OFF);
+
+	/* GPIO on PA17 */
+
+	gpio_set_pin_level(EINK_DC,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(EINK_DC, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(EINK_DC, GPIO_PIN_FUNCTION_OFF);
+
+	/* GPIO on PA23 */
+
+	gpio_set_pin_level(EINK_X_LED_0_PIN,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(EINK_X_LED_0_PIN, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(EINK_X_LED_0_PIN, GPIO_PIN_FUNCTION_OFF);
+
+	/* GPIO on PA24 */
+
+	// Set pin direction to input
+	gpio_set_pin_direction(EINK_X_BUTTON_1_PIN, GPIO_DIRECTION_IN);
+
+	gpio_set_pin_pull_mode(EINK_X_BUTTON_1_PIN,
+	                       // <y> Pull configuration
+	                       // <id> pad_pull_config
+	                       // <GPIO_PULL_OFF"> Off
+	                       // <GPIO_PULL_UP"> Pull-up
+	                       // <GPIO_PULL_DOWN"> Pull-down
+	                       GPIO_PULL_UP);
+
+	gpio_set_pin_function(EINK_X_BUTTON_1_PIN, GPIO_PIN_FUNCTION_OFF);
+
+	/* GPIO on PA25 */
+
+	gpio_set_pin_level(EINK_BUSY,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(EINK_BUSY, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(EINK_BUSY, GPIO_PIN_FUNCTION_OFF);
+
+	/* GPIO on PA26 */
+
+	gpio_set_pin_level(EINK_BS,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(EINK_BS, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(EINK_BS, GPIO_PIN_FUNCTION_OFF);
+
+	/* GPIO on PA29 */
+
+	gpio_set_pin_level(EINK_X_LED_1_PIN,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   true);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(EINK_X_LED_1_PIN, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(EINK_X_LED_1_PIN, GPIO_PIN_FUNCTION_OFF);
+
+	/* GPIO on PB10 */
+
+	gpio_set_pin_level(EINK_FMSDO,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(EINK_FMSDO, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(EINK_FMSDO, GPIO_PIN_FUNCTION_OFF);
+
+	/* GPIO on PB11 */
+
+	gpio_set_pin_level(EINK_MFCSB,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(EINK_MFCSB, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(EINK_MFCSB, GPIO_PIN_FUNCTION_OFF);
+
+	delay_driver_init();
+}
